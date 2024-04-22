c7fd76ec60bc0c887af37dea611d8e18788aba51
grzesiek <grzesiek.soldatowski@gmail.com>
2024-04-22 15:36:23 +0200

    day12 wip

---
+++ src/day12/index.ts
@@ -0,0 +1,51 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n');
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  for (let row of input) {
+    let [springs, pattern] = row.split(' ');
+
+    springs = springs.replaceAll(/\.+/g, '.');
+    springs = springs.replaceAll(/^\.+/g, '');
+    springs = springs.replaceAll(/\.+$/g, '');
+  }
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1`,
+        expected: 21,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: 0,
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});

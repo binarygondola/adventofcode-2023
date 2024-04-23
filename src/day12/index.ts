243bc22bc6e27df25fe3a0a13023c7f809b0d27c
grzesiek <grzesiek.soldatowski@gmail.com>
2024-04-23 17:27:08 +0200

    day12 part 1 wip

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,16 +1,25 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput.split('\n');
+const parseInput = (rawInput: string) => rawInput.split("\n");
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   for (let row of input) {
-    let [springs, pattern] = row.split(' ');
+    let [springs, pattern] = row.split(" ");
 
-    springs = springs.replaceAll(/\.+/g, '.');
-    springs = springs.replaceAll(/^\.+/g, '');
-    springs = springs.replaceAll(/\.+$/g, '');
+    springs = springs.replaceAll(/\.+/g, ".");
+    springs = springs.replaceAll(/^\.+/g, "");
+    springs = springs.replaceAll(/\.+$/g, "");
+
+
+    let parts = springs.split('.');
+    let patterns = pattern.split(',').map(Number);
+    let replaced = patterns.map(x => new Array(x).fill('#').join('')).join('.');
+    console.log(patterns);
+    console.log(springs)
+    console.log(replaced);
+    console.log();
   }
 
   return;
@@ -34,6 +43,10 @@ run({
 ?###???????? 3,2,1`,
         expected: 21,
       },
+      {
+        input: `?????#????? 1,2`,
+        expected: 21,
+      },
     ],
     solution: part1,
   },
@@ -47,5 +60,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });

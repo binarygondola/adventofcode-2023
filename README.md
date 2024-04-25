f0cf549371f316ae4de6c409e4b1fd8bc5c6c7d9
grzesiek <grzesiek.soldatowski@gmail.com>
2024-04-25 23:54:21 +0200

    messy part 1

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
-![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%86/yellow)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -139,16 +139,16 @@ Both parts: 45.531ms
 
 ```
 Day 11
-Time part 1: 12.846ms
-Time part 2: 73.087ms
-Both parts: 85.934ms
+Time part 1: 12.218ms
+Time part 2: 62.899ms
+Both parts: 75.117ms
 ```
 
 ```
 Day 12
-Time part 1: -
+Time part 1: 8647.925ms
 Time part 2: -
-Both parts: -
+Both parts: 8647.925ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 22/50
-Total time: 938652.055ms
+Total stars: 23/50
+Total time: 947289.164ms
 ```
 
 <!--/RESULTS-->
--- src/day12/index.ts
+++ src/day12/index.ts
@@ -5,6 +5,7 @@ const parseInput = (rawInput: string) => rawInput.split("\n");
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
+  let sum = 0;
   for (let row of input) {
     let [springs, pattern] = row.split(" ");
 
@@ -12,17 +13,46 @@ const part1 = (rawInput: string) => {
     springs = springs.replaceAll(/^\.+/g, "");
     springs = springs.replaceAll(/\.+$/g, "");
 
+    let parts = springs.split(".");
+    let patterns = pattern.split(",").map(Number);
+    let replaced = patterns
+      .map((x) => new Array(x).fill("#").join(""))
+      .join(".");
 
-    let parts = springs.split('.');
-    let patterns = pattern.split(',').map(Number);
-    let replaced = patterns.map(x => new Array(x).fill('#').join('')).join('.');
-    console.log(patterns);
-    console.log(springs)
-    console.log(replaced);
-    console.log();
+    let hashCount = springs.split("").reduce((a, c) => (c === "#" ? a + 1 : a), 0);
+    let sumPatterns = patterns.reduce((a, b) => a + b, 0);
+
+    let questionMarks = springs.replaceAll(/[^\?]/g, "");
+
+    let misiingHashes = sumPatterns - hashCount;
+
+    let binary = misiingHashes.toString(2);
+    let maxNum = Math.pow(2, questionMarks.length);
+
+    let num = 0;
+    for (let i = 0; i < maxNum; i++) {
+      let bin = i.toString(2).padStart(questionMarks.length, "0");
+      let newPattern = bin
+        .split("")
+        .map((x, j) => (x === "0" ? '.' : x === "1" ? "#" : x))
+        .join("");
+      if (newPattern.split('').reduce((a, b) => b === '#' ? a + 1 : a, 0) === misiingHashes) {
+        let tmpPattern = [...newPattern];
+        let tmpSprings = springs.split("").map((x, j) => x === "?" ? tmpPattern.shift() : x).join("");
+        tmpSprings = tmpSprings.replaceAll(/\.+/g, ".");
+        tmpSprings = tmpSprings.replaceAll(/^\.+/g, "");
+        tmpSprings = tmpSprings.replaceAll(/\.+$/g, "");
+        let splitted = tmpSprings.replaceAll(/\.+/g, ".").split('.').map(x => x.length);
+        if (splitted.toString() === patterns.toString()) {
+          num++;
+        }
+      }
+    }
+
+    sum += num;
   }
 
-  return;
+  return sum;
 };
 
 const part2 = (rawInput: string) => {
@@ -43,10 +73,10 @@ run({
 ?###???????? 3,2,1`,
         expected: 21,
       },
-      {
-        input: `?????#????? 1,2`,
-        expected: 21,
-      },
+      // {
+      //   input: `?????#????? 1,2`,
+      //   expected: 21,
+      // },
     ],
     solution: part1,
   },

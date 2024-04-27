03a008bccfcea65e2e9e23d4ca576456e071ac8c
grzesiek <grzesiek.soldatowski@gmail.com>
2024-04-27 15:37:44 +0200

    before rewrite

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -6,49 +6,65 @@ const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   let sum = 0;
+  let res = [];
+  let bins = new Array(110).fill(0);
+  let maxNum = 0;
+
   for (let row of input) {
-    let [springs, pattern] = row.split(" ");
+    let [springs, hashPattern] = row.split(" ");
 
-    springs = springs.replaceAll(/\.+/g, ".");
-    springs = springs.replaceAll(/^\.+/g, "");
-    springs = springs.replaceAll(/\.+$/g, "");
+    springs = springs.replaceAll(/\.+/g, ".").replaceAll(/^\.+/g, "").replaceAll(/\.+$/g, "");
 
-    let patterns = pattern.split(",").map(Number);
+    let patterns = hashPattern.split(",").map(Number);
 
     let hashCount = springs.split("").reduce((a, c) => (c === "#" ? a + 1 : a), 0);
-    let sumPatterns = patterns.reduce((a, b) => a + b, 0);
-    let questionMarks = springs.replaceAll(/[^\?]/g, "");
-    let missingHashes = sumPatterns - hashCount;
-    let maxNum = Math.pow(2, questionMarks.length);
+    let hashPatternSum = patterns.reduce((a, b) => a + b, 0);
+    let questionMarksCount = springs.replaceAll(/[^\?]/g, "").length;
+    let missingHashes = hashPatternSum - hashCount;
+    let maxCombination = Math.pow(2, questionMarksCount);
 
     let num = 0;
-    for (let i = 0; i < maxNum; i++) {
-      let bin = i.toString(2).padStart(questionMarks.length, "0");
-      let newPattern = bin
-        .split("")
-        .map((x, j) => (x === "0" ? '.' : x === "1" ? "#" : x))
-        .join("");
-      if (newPattern.split('').reduce((a, b) => b === '#' ? a + 1 : a, 0) === missingHashes) {
+    for (let i = 0; i < maxCombination; i++) {
+      let binaryNum = i.toString(2).padStart(questionMarksCount, "0");
+      let newPattern = binaryNum.split("").map(x => (x === "0" ? '.' : '#')).join("");
+      let newPatternHashSum = newPattern.split("").reduce((a, b) => b === '#' ? a + 1 : a, 0);
+
+      if (newPatternHashSum === missingHashes) {
         let tmpPattern = [...newPattern];
-        let tmpSprings = springs.split("").map((x, j) => x === "?" ? tmpPattern.shift() : x).join("");
-        tmpSprings = tmpSprings.replaceAll(/\.+/g, ".");
-        tmpSprings = tmpSprings.replaceAll(/^\.+/g, "");
-        tmpSprings = tmpSprings.replaceAll(/\.+$/g, "");
-        let splitted = tmpSprings.replaceAll(/\.+/g, ".").split('.').map(x => x.length);
-        if (splitted.toString() === patterns.toString()) {
+        let tmpSprings = springs.split("").map(x => x === "?" ? tmpPattern.shift() : x).join("");
+        tmpSprings = tmpSprings.replaceAll(/\.+/g, ".").replaceAll(/^\.+/g, "").replaceAll(/\.+$/g, "");
+        let newHashPattern = tmpSprings.split('.').map(x => x.length);
+        if (newHashPattern.toString() === patterns.toString()) {
           num++;
         }
       }
     }
 
+    res.push(num);
+    bins[num] += 1;
+    maxNum = Math.max(maxNum, num);
     sum += num;
   }
+  console.log(maxNum);
+  console.log(bins.slice(0, 50));
+  console.log(bins.slice(51, 110));
 
   return sum;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+
+  for (let row of input) {
+    let [springsPattern, hashPattern] = row.split(" ");
+
+    springsPattern = new Array(5).fill(springsPattern).join("?");
+    hashPattern = new Array(5).fill(hashPattern).join(",");
+
+    springsPattern = springsPattern.replaceAll(/\.+/g, ".").replaceAll(/^\.+/g, "").replaceAll(/\.+$/g, "");
+
+    // console.log(springsPattern);
+  }
 
   return;
 };
@@ -65,19 +81,25 @@ run({
 ?###???????? 3,2,1`,
         expected: 21,
       },
-      // {
-      //   input: `?????#????? 1,2`,
-      //   expected: 21,
-      // },
+      {
+        input: '????????????????????? 2,1,2,1',
+        expected: 0,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: 0,
-      // },
+      {
+        input: `.# 1
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1`,
+        expected: 525152,
+      },
     ],
     solution: part2,
   },

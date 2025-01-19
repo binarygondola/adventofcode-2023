9910d5c4d4beb2dbe82914cbe95607dd7e8b8391
grzesiek <grzesiek.soldatowski@gmail.com>
2025-01-19 14:33:59 +0100

    added day15 part2

--- README.md
+++ README.md
@@ -25,7 +25,7 @@
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%86/yellow)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
-[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: 879.75ms
 
 ```
 Day 15
-Time part 1: 2.381ms
-Time part 2: -
-Both parts: 2.381ms
+Time part 1: 2.422ms
+Time part 2: 5.179ms
+Both parts: 7.602ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 28/50
-Total time: 948664.432ms
+Total stars: 29/50
+Total time: 948669.653ms
 ```
 
 <!--/RESULTS-->
--- src/day15/index.ts
+++ src/day15/index.ts
@@ -6,39 +6,61 @@ const hash = (asciiCode: number, val: number) => {
   return (val + asciiCode) * 17 % 256;
 }
 
+const HASH = (op: string) => {
+  return op.split('').reduce((acc, c) => hash(c.charCodeAt(0), acc), 0);
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  let sum = 0;
-  for (let step of input) {
-    sum += step.split('').reduce((acc, c) => hash(c.charCodeAt(0), acc), 0);
-  }
-
-  return sum;
+  return input.reduce((acc, curr) => acc + HASH(curr), 0);;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  let boxes: Map<string, number>[] = new Array(256).fill(0).map(_ => new Map());
+
+  for (let op of input) {
+    let label, focalLength, box;
+    if (op.split('').includes('-')) {
+      label = op.slice(0, -1);
+    } else {
+      [label, focalLength] = op.split('=');
+      focalLength = +focalLength;
+    }
+
+    box = HASH(label);
+
+    if (focalLength) {
+      boxes[box].set(label, focalLength);
+    } else {
+      boxes[box].delete(label);
+    }
+  }
+
+
+  let hashmapValues = [...boxes.entries()].map(hashmap => [...hashmap[1].values()]);
+  let partialSums = hashmapValues.map((e, index) => e.reduce((acc, curr, idx) => acc + (index + 1) * curr * (idx + 1), 0));
+  return partialSums.reduce((acc, curr) => acc + curr, 0);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: 0,
-      // },
+      {
+        input: `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+        expected: 1320,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: 0,
-      // },
+      {
+        input: `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+        expected: 145,
+      },
     ],
     solution: part2,
   },

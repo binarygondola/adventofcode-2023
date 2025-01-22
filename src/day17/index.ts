4059aae3d6a8a51ca48fe830828cde0a381d8cd2
grzesiek <grzesiek.soldatowski@gmail.com>
2025-01-22 22:37:26 +0100

    added basic traversing for part 1, better heuristic needed

--- src/day17/index.ts
+++ src/day17/index.ts
@@ -2,51 +2,72 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput.split('\n');
 
-type candidateType = { x: number; y: number; heat: number; };
+type position = { x: number; y: number };
 
-const bfs = (vis: number[][], candidates: candidateType[], input: string[][]) => {
-  while (candidates.length > 0) {
-    let c = candidates.shift();
-    if (c && c.y >= 0 && c.y < vis.length && c.x >= 0 && c.x < vis[0].length) {
-      let i = +input[c.y][c.x];
+type candidateType = { pos: position; heat: number; previous: [position, position, position], steps: number };
 
-      if (vis[c.y][c.x] === 0) {
-        vis[c.y][c.x] = c.y * 1000 + c.x;
-        candidates.push({
-          x: c.x + 1,
-          y: c.y,
-          heat: c.heat + i
-        });
-        candidates.push({
-          x: c.x,
-          y: c.y + 1,
-          heat: c.heat + i
-        });
-        candidates.push({
-          x: c.x - 1,
-          y: c.y,
-          heat: c.heat + i
-        });
-        candidates.push({
-          x: c.x,
-          y: c.y - 1,
-          heat: c.heat + i
-        });
-        candidates.sort((a, b) => a.heat - b.heat);
-
-        bfs(vis, candidates, input);
-      }
-    }
+const getCandidate = (prev: candidateType, moveVec: position, currentPos: position, heat: number): candidateType => {
+  return {
+    pos: {
+      x: prev.pos.x + moveVec.x,
+      y: prev.pos.y + moveVec.y,
+    },
+    previous: [currentPos, ...prev.previous.slice(0, 2) as [position, position]],
+    heat: prev.heat + heat,
+    steps: prev.steps + 1,
   }
+}
+
+const isValidCandidate = (candidate: candidateType, dimX: number, dimY: number, vis: number[][]) => {
+  let pos = candidate.pos;
+  if (pos.y === candidate.previous[1].y && pos.x === candidate.previous[1].x) return false;
+  let xs = new Set(candidate.previous.map(p => p.x));
+  if (xs.size === 1 && pos.x === candidate.previous[0].x) return false;
+  let ys = new Set(candidate.previous.map(p => p.y));
+  if (ys.size === 1 && pos.y === candidate.previous[0].y) return false;
+  if (pos.y < 0 || pos.y >= dimY || pos.x < 0 || pos.x >= dimX) return false;
+  return true;
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput).map(r => r.split(''));
 
-  let vis: number[][] = new Array(input.length).fill(0).map(r => new Array(input[0].length).fill(0));
-  let candidates = [];
-  candidates.push({ x: 0, y: 0, heat: 0 })
-  bfs(vis, candidates, input);
+  let vis: number[][] = new Array(input.length).fill(0).map(r => new Array(input[0].length).fill(1000000));
+  let candidates: candidateType[] = [];
+  candidates.push({ pos: { x: 0, y: 0 }, heat: 0, previous: [{ x: -1, y: -1 }, { x: -2, y: -2 }, { x: -3, y: -3 }], steps: 0 })
+
+  let found = false;
+
+  let counter = 10000;
+
+  let dimX = input[0].length;
+  let dimY = input.length;
+
+  while (candidates.length > 0 && !found && counter-- > 0) {
+    let candidate = candidates.shift()!;
+    // console.log(candidate.steps, candidate.heat, candidate.pos, '---', candidate.previous.map(p => [p.x, p.y]).join(', '));
+    let pos = candidate.pos;
+    let heat = +input[pos.y][pos.x];
+
+    if (candidate.pos.x === dimX - 1 && candidate.pos.y === dimY - 1) {
+      console.log('found ', candidate);
+      vis[candidate.pos.y][candidate.pos.x] = Math.min(vis[candidate.pos.y][candidate.pos.x], candidate.heat + heat);
+      found = true;
+      continue;
+    }
+
+
+    [[0, 1], [1, 0], [0, -1], [-1, 0]].forEach(v => {
+      let c = getCandidate(candidate, { x: v[0], y: v[1] }, pos, heat);
+      if (isValidCandidate(c, dimX, dimY, vis)) candidates.push(c)
+    })
+
+    vis[pos.y][pos.x] = Math.min(vis[pos.y][pos.x], candidate.heat + heat);
+
+    candidates.sort((a, b) => a.steps - b.steps);
+  }
+
+  // console.log(vis.join('\n'));
 
   return;
 };
@@ -77,8 +98,10 @@ run({
         expected: 102,
       },
       {
-        input: `12
-34`,
+        input: `1234
+2345
+3456
+4567`,
         expected: 6
       }
     ],
